039e931cd77d231e6db26444c2d61ba78a892195
joy <aa012003@gmail.com>
2024-04-14 18:39:21 +0800

    修改

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/production-sub-path/' : '/',
+  base: '/togo/',
   plugins: [
     vue(),
     eslintPlugin({
